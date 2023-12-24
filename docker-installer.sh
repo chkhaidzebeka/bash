4424a57307f38c7b20268a65df915dd8d9f8b987
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2023-12-24 20:00:41 +0400

    Update docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -25,3 +25,23 @@ sudo systemctl enable docker
 sudo addgroup wheel
 sudo usermod -aG wheel $USER
 sudo usermod -aG docker $USER
+
+
+# Install k8s
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+
+# Install help
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install rke
+curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep download_url | grep amd64 | cut -d '"' -f 4 | wget -qi - 
+chmod +x rke_linux-amd64 
+sudo mv rke_linux-amd64 /usr/local/bin/rke 
+rke --version
